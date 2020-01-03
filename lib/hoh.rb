514a754cc98133b654686cd850d54d3a25183170
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 12:24:37 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -35,8 +35,8 @@ def naming_system
   label: "Kingdom",
    sub_category: {
      label: "Phylum",
-    sub_category: {
-       label: "Class"
+      sub_category: {
+        label: "Class"
      }
    }
   }
